809ac2857422cf556fb6e05ab55b19123ba6c45e
romandubinin <Roman230394@yandex.ru>
2014-10-16 16:36:38 +0600

    замутил в один файл, как того требуют правила

--- Jaccar/Jaccar/JaccarMaesure.cs
+++ Jaccar/Jaccar/JaccarMaesure.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Text.RegularExpressions;
+using System.IO;
 
 namespace Jaccar
 {
@@ -49,7 +50,6 @@ namespace Jaccar
         }
     }
 
-
     public class JaccarMeasure
     {
         public string[] ToSentences(string text)
@@ -89,6 +89,37 @@ namespace Jaccar
                 }
             }
             return AllNGramms.ToArray();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            JaccarMeasure j = new JaccarMeasure();
+            string text1 = String.Concat(File.ReadAllLines(args[0]));
+            string text2 = String.Concat(File.ReadAllLines(args[1]));
+            int n = int.Parse(args[2]);
+
+            var text1NGramms = j.NGrammsFromText(text1, n);
+            var text2NGramms = j.NGrammsFromText(text2, n);
+            var allNGrams = text1NGramms.Concat(text2NGramms)
+                .GroupBy(g => g, new NGram())
+                .Select(group => group.Key)
+                .ToArray();
+
+            int common = 0;
+            int total = 0;
+
+            foreach (var nG in allNGrams)
+            {
+                common += Math.Min(text1NGramms.Count(x => x == nG), text2NGramms.Count(x => x == nG));
+                total += Math.Max(text1NGramms.Count(x => x == nG), text2NGramms.Count(x => x == nG));
+            }
+
+            Console.WriteLine(text1NGramms.Count());
+            Console.WriteLine(text2NGramms.Count());
+            Console.WriteLine(common / (double)total);
         }
     }
 }
