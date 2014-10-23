49af6d3e54f7d0850105ed8ac1a1dc170e148195
romandubinin <Roman230394@yandex.ru>
2014-10-23 11:35:06 +0600

    добавил тестовый класс(для проверки работы кнопок), собрал всё воедино

---
+++ PlatformManager/PlatformManager/TestMachine.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlatformManager
+{
+    class TestMachine: IMachine
+    {
+        public void Up()
+        {
+            Console.WriteLine("UP");
+        }
+
+        public void Down()
+        {
+            Console.WriteLine("Down");
+        }
+
+        public void Left()
+        {
+            Console.WriteLine("Left");
+        }
+
+        public void Right()
+        {
+            Console.WriteLine("Right");
+        }
+    }
+}
